56ee9aa4c0b65cefe6d0ad3a02ab907fe8dd21b3
Peter Bengtsson <mail@peterbe.com>
2020-02-28 10:59:58 -0500

    fixes for proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,13 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "http://localhost:5000",
-      changeOrigin: true
-    })
-  );
+  if (process.env.NODE_ENV === "development") {
+    app.use(
+      "/api",
+      createProxyMiddleware({
+        target: "http://localhost:5000",
+        changeOrigin: true
+      })
+    );
+  }
 };
