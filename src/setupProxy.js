d9fce178206ee37a32a0d0a28b7a793ed4589a85
Peter Bengtsson <mail@peterbe.com>
2020-02-28 16:35:11 -0500

    upgrade things

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,12 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-if (process.env.NODE_ENV === "development") {
-  console.log("Setting up a Proxy to localhost:5000");
-  module.exports = function(app) {
-    app.use(
-      "/api",
-      createProxyMiddleware({
-        target: "http://localhost:5000",
-        changeOrigin: true
-      })
-    );
-  };
-} else {
-  console.log("Not in development, so no proxy server");
-}
+console.log("Setting up a Proxy to localhost:5000");
+module.exports = function(app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://localhost:5000",
+      changeOrigin: true
+    })
+  );
+};
