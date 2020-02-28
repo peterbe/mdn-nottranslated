747cc2373ea4480733c43fdd637d09bc69daec5a
Peter Bengtsson <mail@peterbe.com>
2020-02-28 11:11:35 -0500

    does this work?

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+console.log({ NODE_ENV: process.env.NODE_ENV }); // debugging!
+
 if (process.env.NODE_ENV === "development") {
   console.log("Setting up a Proxy to localhost:5000");
   module.exports = function(app) {
