b61b77a880d89cc9b0b83b39c006b2ec7f66d8bc
Schalk Neethling <sneethling@mozilla.com>
2020-03-02 10:17:13 +0200

    Some copy updates

--- src/App.js
+++ src/App.js
@@ -102,16 +102,16 @@ function Head() {
               </h1>
               {atHome && (
                 <h2 className="subtitle is-4">
-                  Help us review localized MDN that was never fully translated.
+                  Help us review localized MDN content that was never fully translated.
                 </h2>
               )}
               {atHome ? (
                 <h3 className="subtitle is-5">
                   Over the years,{" "}
                   <a href="https://developer.mozilla.org">MDN Web Docs</a>
-                  unfortunately has a lot of localized content whose translation
-                  effort started but never finished. Now, we have to get rid of
-                  these, but it's hard to automate with confidence. So that's
+                  ,unfortunately, accumulated a lot of localized content whose translation
+                  efforts started but never finished. Now, we have to get rid of
+                  these, but it’s hard to automate with confidence. So that’s
                   where you come in.
                 </h3>
               ) : (
@@ -493,7 +493,7 @@ function About() {
           <a href="https://github.com/peterbe/mdn-nottranslated">
             github.com/peterbe/mdn-nottranslated
           </a>{" "}
-          and it's wide open of suggestions, but reports, and contributions.
+          and is wide open for suggestions, bug reports, and contributions.
         </b>
       </p>
       <p>
@@ -501,16 +501,16 @@ function About() {
         <a href="https://developer.mozilla.org">MDN Web Docs</a> better by
         getting rid of unfinished and severely incomplete attempts at
         translating documents. MDN is a Wiki and to translate a page, the very
-        first thing it does is to <i>clone</i>
-        the English (US) document and then you can start overwriting the English
-        with whatever language you're intending to translate to. Over the years,
-        what we've come to discover is that a lot of these attempts are
+        first thing the wiki does is to <i>clone</i>
+        the English (US) document and from there you can start overwriting the English
+        with whatever language you’re intending to translate it to. Over the years,
+        what we’ve come to discover is that a lot of these attempts are
         incomplete.{" "}
         <b>
           The document is actually <i>not</i> translated
         </b>
-        . So, let's figure out which those implement documents are so we can
-        delete them and make way for newer and better translations.
+        . So, let’s figure out which documents are in an incomplate state so
+        we can delete them and make way for newer and better translations.
       </p>
 
       <p style={{ textAlign: "center" }}>
@@ -521,9 +521,9 @@ function About() {
 
       <p>
         At the moment, this web app is just for <i>highlighting</i> which
-        documents are actually not translated. To do something about it you need
-        access permission on <code>wiki.developer.mozilla.org</code> to do
-        something about the document. Perhaps this can change in the future.
+        documents are not translated. To do something about it, you need
+        to create an account on <code>wiki.developer.mozilla.org</code> and
+        complete the translation. Perhaps this can change in the future.
       </p>
     </div>
   );
