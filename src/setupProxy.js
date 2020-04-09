66ab0d36771f54ba895b21422423ac186d125366
Peter Bengtsson <mail@peterbe.com>
2020-04-09 09:30:53 -0400

    Pretty (#30)
    * pretty
    
    * lint then build
    
    * formatted

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,12 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 console.log("Setting up a Proxy to localhost:5000");
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
       target: "http://localhost:5000",
-      changeOrigin: true
+      changeOrigin: true,
     })
   );
 };
